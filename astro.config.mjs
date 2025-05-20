dbab74ce9682cc28f7979f8ca51d62d22d05f43e
xhiena <xhiena@gmail.com>
2025-05-20 16:44:15 +0200

    remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://pablo.martinez-perez/blog"
+    site: "https://pablo.martinez-perez/",
+    base: ''
 });
