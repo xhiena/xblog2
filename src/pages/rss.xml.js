9b247e7de6fb8de5980f26ef693f5c2080dbcf85
xhiena <xhiena@gmail.com>
2025-05-23 14:47:10 +0200

    rss changes

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,8 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
     return rss({
-        title: 'Astro Learner',
-        description: 'My journey learning Astro',
+        title: "Pablo's Blog",
+        description: 'My blog about everything and nothing',
         site: context.site,
         items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
         customData: `<language>en-gb</language>`,
